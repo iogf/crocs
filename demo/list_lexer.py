646187f165b76ab25bb827cdc27985476ee8c269
Iury O. G. Figueiredo <last.src@gmail.com>
2019-12-24 05:56:27 +0000

    Adding examples.

--- demo/list_lexer.py
+++ demo/list_lexer.py
@@ -15,39 +15,32 @@ class ListTokens:
     LexChain(lexmap, LexNode('[0-9]+', Token))
     LexChain(lexmap, LexNode(' +', Token))
 
-# print('Example 1')
-# lex = Lexer(ListTokens.lexmap)
-# data = '[]'
-# lex.feed(data)
-# tokens = lex.run()
-# print('Consumed:', list(tokens))
-
-# print('Example 2')
-# lex = Lexer(ListTokens.lexmap)
-# data = '[1, [2, [1,]]]'
-# lex.feed(data)
-# tokens = lex.run()
-# print('Consumed:', list(tokens))
-# 
-# print('Example 3')
-# lex = Lexer(ListTokens.lexmap)
-# data = '[1, ss]'
-# lex.feed(data)
-# tokens = lex.run()
-# print('Consumed:', list(tokens))
-# 
-# print('Example 4')
-# lex = Lexer(ListTokens.lexmap)
-# data = '[22,2, [, ]]'
-# lex.feed(data)
-# tokens = lex.run()
-# print('Consumed:', list(tokens))
-
-print('Example 5')
+print('Example 1')
 lex = Lexer(ListTokens.lexmap)
-data = '[1 2]'
+data = '[]'
+lex.feed(data)
+tokens = lex.run()
+print('Consumed:', list(tokens))
+
+print('Example 2')
+lex = Lexer(ListTokens.lexmap)
+data = '[1 [1 2 [1 2 [ 2] 3] 4] ]'
 lex.feed(data)
 tokens = lex.run()
 print('Consumed:', list(tokens))
 
 
+print('Example 3')
+lex = Lexer(ListTokens.lexmap)
+data = '[1 [] 3 []]'
+lex.feed(data)
+tokens = lex.run()
+print('Consumed:', list(tokens))
+
+# Should fail.
+print('Example 4')
+lex = Lexer(ListTokens.lexmap)
+data = '[1 [[ 3 []]'
+lex.feed(data)
+tokens = lex.run()
+print('Consumed:', list(tokens))
