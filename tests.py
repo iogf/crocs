1c596abbbf8224fd728fd8125ab99908b45c64fe
Iury O. G. Figueiredo <last.src@gmail.com>
2020-06-27 02:19:09 +0000

    Fixing setup.py version, adding test classes.

--- tests.py
+++ tests.py
@@ -1,5 +1,6 @@
 import unittest
-from crocs.regex import Include
+from crocs.regex import Include, Exclude, Any, OneOrZero, \
+OneOrMore, Group, ConsumeNext, ConsumeBack, X, Join, Seq
 
 class TestInclude(unittest.TestCase):
     def setUp(self):
@@ -7,6 +8,98 @@ class TestInclude(unittest.TestCase):
 
     def test0(self):
         pass
+
+class TestExclude(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test0(self):
+        pass
+
+class TestAny(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test0(self):
+        pass
+
+class TestOneOrZero(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test0(self):
+        pass
+
+class TestOneOrMore(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test0(self):
+        pass
+
+class TestGroup(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test0(self):
+        pass
+
+class TestNamedGroup(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test0(self):
+        pass
+
+class TestRepeat(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test0(self):
+        pass
+
+class TestAsk(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test0(self):
+        pass
+
+class TestConsumeNext(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test0(self):
+        pass
+
+class TestConsumeBack(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test0(self):
+        pass
+
+class TestSeq(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test0(self):
+        pass
+
+class TestDot(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test0(self):
+        pass
+
+class TestJoin(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test0(self):
+        pass
+
 
 if __name__ == '__main__':
     unittest.main()
