9626c30e4026ff3ec38fec0461a89f0bcf79be1c
Iury O. G. Figueiredo <last.src@gmail.com>
2020-06-29 09:26:21 +0000

    Fixing setup version.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 
 from distutils.core import setup
 setup(name="crocs",
-      version="2.3.0",
+      version="2.4.0",
       packages=["crocs", ],
       scripts = ['regxhits'],
       author="Iury O. G. Figueiredo",
